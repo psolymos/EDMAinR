ca8599d202b68191e0d4dfe2b69bd3bb9c99a61d
Peter Solymos <psolymos@gmail.com>
2019-05-31 09:29:50 -0600

    1st batch of files

---
+++ R/edma_fit.R
@@ -0,0 +1,213 @@
+## workhorse function to estimate centered mean form and SigmaKstar
+## X: read.table(x) # data frame input, n tables (each K x D) stacked ???
+## n: number of individuals (replicates)
+## K: number of landmarks
+## D: dimensions
+edma_fit <- function(X, n, K, D) {
+    ## step1: calculate Euclidean distance for X
+    d <- function(x, y) {
+        sum((x - y)^2)
+    }
+    EuX <- matrix(nrow = n * K, ncol = K)
+    for (i in 1:n) {
+        for (j in ((i - 1) * K + 1):(i * K)) {
+            for (l in ((i - 1) * K + 1):(i * K)) {
+                EuX[j, (l - (i - 1) * K)] <- d(X[j, ], X[l, ])
+            }
+        }
+    }
+    ## EuX step 2 calculate Eu(M)
+    EuM <- matrix(nrow = K, ncol = K)
+    for (i in 1:K) {
+        for (j in 1:K) {
+            sum1 <- 0
+            for (l in 1:n) {
+                sum1 <- sum1 + EuX[((l - 1) * K + i), j]
+            }
+            mean.squ.eudis <- sum1/n
+            sum2 <- 0
+            for (l in 1:n) {
+                sum2 <- sum2 + (EuX[((l - 1) * K + i), j] - mean.squ.eudis)^2
+            }
+            var.squ.eudis <- sum2/n
+            if (D == 2) {
+            	EuM[i, j] <- sqrt((mean.squ.eudis)^2 - var.squ.eudis)
+            } else {
+            	EuM[i, j] <- sqrt((mean.squ.eudis)^2 - 1.5 * var.squ.eudis)
+            }
+        }
+    }
+    EuM
+    ## step 3 calculate B(M)
+    I <- diag(1, K)
+    ones <- array(rep(1, K), c(1, K))
+    H <- I - (1/K) * crossprod(ones, ones)
+    BM <- (-1/2) * H %*% EuM %*% H
+    ## step 4 calculate eigenvalues and eigenvectors of B(M)
+    eigen(BM)
+    ## step 5 estimate centred mean form M
+    est.M <- matrix(nrow = K, ncol = D)
+    for (i in 1:D) {
+        est.M[, i] <- sqrt(eigen(BM)$values[i]) * eigen(BM)$vectors[, i]
+    }
+    ## step 6 estimate SigmaKstar=H*SigmaK*H
+    BX <- matrix(nrow = n * K, ncol = K)
+    for (i in 1:n) {
+        BX[((i - 1) * K + 1):(i * K), ] <- (-1/2) * H %*% EuX[((i - 1) *
+            K + 1):(i * K), ] %*% H
+    }
+    a <- 0
+    for (i in 1:n) {
+        a <- a + BX[((i - 1) * K + 1):(i * K), ]
+    }
+    est.SigmaKstar <- (a/n - BM)/D
+    ## compare HM with est.M ; HMt(M)H with est.M*t(est.M); H*SigmaK*H with
+    #est.SigmaKstar
+    #H %*% Meanform
+    #est.M
+    #H %*% Meanform %*% t(Meanform) %*% H
+    #est.M %*% t(est.M)
+    #H %*% SigmaK %*% H
+    #est.SigmaKstar
+    ## Use maple to solve Y such that YH=L
+    fcol <- array(c(rep(-1, K - 2), -2), c(K - 1, 1))
+    identity <- diag(1, K - 2, K - 1)
+    augment <- array(c(rep(-1, K - 2), 0), c(1, K - 1))
+    Y <- cbind(fcol, rbind(identity, augment))
+    Y
+    ## from est.SigmaKstar to est.SigmaK~
+    est.SigmaKtilde <- Y %*% est.SigmaKstar %*% t(Y)
+    est.SigmaKtilde
+    L <- cbind(c(rep(-1, K - 1)), diag(1, K - 1))
+    ## L%*%SigmaK%*%t(L) # model for diagonal matrix
+    c <- as.vector(est.SigmaKtilde)
+    A <- matrix(nrow = (K - 1)^2, ncol = K)
+    iden <- diag(1, K - 1)
+    for (i in 1:(K - 1)) {
+        A[(i - 1) * K + 1, ] <- c(1, iden[i, ])
+    }
+    for (i in 1:(K - 2)) {
+        A[((i - 1) * K + 2):(i * K), ] <- matrix(rep(c(1, rep(0, K - 1)),
+            K - 1), nrow = K - 1, byrow = T)
+    }
+    ## A
+    est.vect <- round((solve(t(A) %*% A)) %*% t(A) %*% c, 3)
+    est.SigmaK <- diag(c(est.vect))
+    ## general case
+    c <- as.vector(est.SigmaKtilde)
+    b.ori <- as.vector(SigmaK)
+    m <- 0
+    b.dis <- rep(0, K * (K + 1)/2)
+    for (i in 1:K) {
+        for (j in i:K) {
+            m <- m + 1
+            b.dis[m] <- SigmaK[i, j]
+        }
+    }
+    d <- 0
+    for (i in 1:(K * (K + 1)/2)) {
+        if (b.dis[i] != 0)
+            d <- c(d, b.dis[i])
+    }
+    b <- d[-1]
+    b
+    c.dis <- rep(0, K * (K - 1)/2)
+    q <- 0
+    for (i in 1:(K - 1)) {
+        for (j in i:(K - 1)) {
+            q <- q + 1
+            c.dis[q] <- est.SigmaKtilde[i, j]
+        }
+    }
+    iden <- diag(1, K - 1)
+    A <- matrix(nrow = (K - 1)^2, ncol = K^2)
+    A[, K + 1] <- 0
+    for (i in 1:(K - 1)) {
+        A[, 1] <- 1
+        A[((i - 1) * (K - 1) + 1):(i * (K - 1)), 2:K] <- diag(-1, K - 1)
+        A[((i - 1) * (K - 1) + 1):(i * (K - 1)), i + 1] <- -1
+        A[(i - 1) * K + 1, i + 1] <- -2
+        A[((i - 1) * (K - 1) + 1):(i * (K - 1)), (i * K + 2):((i + 1) *
+            K)] <- iden
+        A[((i - 1) * (K - 1) + 1):(i * (K - 1)), c(-(1:(K + 1)), -((i *
+            K + 2):((i + 1) * K)))] <- 0
+    }
+    pcol <- rep(0, K * (K - 1)/2)
+    t <- 1
+    for (i in 1:(K - 1)) {
+        for (j in 1:i) {
+            pcol[t] <- i * K + j
+            t <- t + 1
+        }
+    }
+    B <- matrix(nrow = (K - 1)^2, ncol = K * (K + 1)/2)
+    pcol.new <- c(pcol, 0)
+    i <- 1
+    ColB <- 1
+    for (ColA in 1:K^2) {
+        if (ColA == pcol.new[i])
+            i <- i + 1 else {
+            B[, ColB] <- A[, ColA]
+            ColB <- ColB + 1
+        }
+    }
+    prow <- rep(0, (K - 2) * (K - 1)/2)
+    t <- 1
+    for (i in 1:(K - 2)) {
+        for (j in 1:i) {
+            prow[t] <- i * (K - 1) + j
+            t <- t + 1
+        }
+    }
+    C <- matrix(nrow = K * (K - 1)/2, ncol = K * (K + 1)/2)
+    prow.new <- c(prow, 0)
+    i <- 1
+    RowC <- 1
+    for (RowB in 1:(K - 1)^2) {
+        if (RowB == prow.new[i])
+            i <- i + 1 else {
+            C[RowC, ] <- B[RowB, ]
+            RowC <- RowC + 1
+        }
+    }
+    Q <- 0
+    for (i in 1:(K * (K + 1)/2)) {
+        if (b.dis[i] != 0)
+            Q <- cbind(Q, C[, i])
+    }
+    A.aug <- Q[, -1]
+    A.aug
+    ## calculate the rank of a matrix(by using SVD)
+    matrix.rank <- function(A, eps = .Machine$double.eps) {
+        sv. <- abs(svd(A)$d)
+        sum((sv./max(sv.)) > eps)
+    }
+    matrix.rank(A.aug)
+    Kstar <- length(b)
+    Kstar
+    if (nrow(A.aug) != ncol(A.aug)) {
+        est.vect <- solve(A.aug, c.dis)
+    } else if (ncol(A.aug) == Kstar) {
+        est.vect <- (solve(t(A.aug) %*% A.aug)) %*% t(A.aug) %*% c.dis
+    } else stop(" SigmaK is not identifiable\n")
+    ## general case est.SigmaK
+    est.SigmaK <- matrix(nrow = K, ncol = K)
+    t <- 1
+    for (i in 1:K) {
+        for (j in i:K) {
+            if (SigmaK[i, j] == 0) {
+                est.SigmaK[i, j] <- 0
+                est.SigmaK[j, i] <- 0
+            } else {
+                est.SigmaK[i, j] <- est.vect[t]
+                est.SigmaK[j, i] <- est.vect[t]
+                t <- t + 1
+            }
+        }
+    }
+    out <- list()
+    out$MMt <- BM
+    out$M <- est.M
+    out$SigmaK <- est.SigmaK
+    out
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# EDMAinR
-Euclidean Distance Matrix Analysis in R
+# EDMAinR - Euclidean Distance Matrix Analysis in R
+
+> A coordinate‐free approach for comparing biological shapes using landmark data
+
+```R
+devtools::install_github("psolymos/EDMAinR")
+```
