af30848fc26952ea8f1fddf8168f84f38a7074ee
Peter Solymos <psolymos@gmail.com>
2019-11-27 15:11:20 -0700

    Update edma_par.R

--- inst/doc/edma_par.R
+++ inst/doc/edma_par.R
@@ -24,35 +24,6 @@
     x
 }
 
-.SigmaK_fit <- function(SigmaKstar, H, pattern, init,
-method = "Nelder-Mead", control = list(), hessian = FALSE) {
-    K <- nrow(SigmaKstar)
-    fac <- .mat2fac(pattern)
-    p <- nlevels(droplevels(fac))
-    if (p > K*(K-1)/2)
-        stop(
-            sprintf(
-                "pattern with %s unknowns implies non estimable SigmaK",
-            p))
-    if (missing(init))
-        init <- structure(numeric(p), names=levels(fac))
-    num_max <- .Machine$double.xmax^(1/3)
-    ## we might need constraints here, i.e. >0 diag values
-    fun <- function(parms){
-        SigmaK <- .vec2mat(parms, fac)
-        if (any(diag(SigmaK)) < 0)
-            return(num_max)
-        10^4 * max((SigmaKstar - (H %*% SigmaK %*% t(H)))^2)
-    }
-    if (!is.null(control$fnscale) && control$fnscale < 0)
-        stop("control$fnscale can not be negative")
-    o <- suppressWarnings({
-        optim(init, fun, method=method, control=control, hessian=hessian)
-    })
-    o$SigmaK <- .vec2mat(o$par, fac)
-    o
-}
-
 ## simulate stuff -- this is already there as edma_simulate_data !!!
 .edma_simulate <- function(n, M, SigmaK) {
     D <- ncol(M)
@@ -112,13 +83,31 @@ parm <- c(a=0.25)
 ## this works
 m <- matrix(c(
     "a", NA, NA, NA,
+    NA, "a", NA, NA,
+    NA,  NA, "b", NA,
+    NA,  NA, NA, "b"
+), 4, 4, byrow=TRUE)
+parm <- c(a=0.25, b=0.35)
+
+# this worked
+m <- matrix(c(
+    "a", NA, NA, NA,
+    NA, "b", NA, NA,
+    NA,  NA, "c", NA,
+    NA,  NA, NA, "d"
+), 4, 4, byrow=TRUE)
+parm <- c(a=0.25, b=0.35, c=0.1, d=0.05)
+
+## this works
+m <- matrix(c(
+    "a", NA, NA, NA,
     "b", "a", NA, NA,
     NA,  NA, "a", NA,
     NA,  NA, "b", "a"
 ), 4, 4, byrow=TRUE)
 parm <- c(a=0.15, b=0.05)
 
-## this works
+## this does not works
 m <- matrix(c(
     "a", NA, NA, NA,
     "b", "a", NA, NA,
@@ -127,12 +116,46 @@ m <- matrix(c(
 ), 4, 4, byrow=TRUE)
 parm <- c(a=0.25, b=0.07)
 
-M <- structure(c(-2.5, 7.5, -2.5, -2.5, -7.5, 2.5, 2.5, 2.5),
+.SigmaK_fit <- function(SigmaKstar, H, pattern, init,
+method = "Nelder-Mead", control = list(), hessian = FALSE) {
+    ## test that SigmaKstar is of the right (K-1) rank
+    K <- nrow(SigmaKstar)
+    fac <- .mat2fac(pattern)
+    p <- nlevels(droplevels(fac))
+    if (p > K*(K-1)/2)
+        stop(
+            sprintf(
+                "pattern with %s unknowns implies non estimable SigmaK",
+            p))
+    if (missing(init))
+        init <- structure(numeric(p), names=levels(fac))
+    num_max <- .Machine$double.xmax^(1/3)
+    ## we might need constraints here, i.e. >0 diag values
+    fun <- function(parms){
+        SigmaK <- .vec2mat(parms, fac)
+        if (any(diag(SigmaK) <= 0))
+            return(num_max)
+        10^4 * max((SigmaKstar - (H %*% SigmaK %*% H))^2)
+    }
+    if (!is.null(control$fnscale) && control$fnscale < 0)
+        stop("control$fnscale can not be negative")
+    o <- suppressWarnings({
+        optim(init, fun, method=method, control=control, hessian=hessian)
+    })
+    o$SigmaK <- .vec2mat(o$par, fac)
+    o
+}
+
+M <- structure(c(-2.5, 7.5, -2.5, -2.5, -7.5, 2.5, 2.5, 4.5),
     .Dim = c(4L, 2L))
 SigmaK <- .vec2mat(parm, .mat2fac(m))
 sim <- .edma_simulate(n=1000, M, SigmaK)
 
-fit <- .edma_fit_np(sim$X, sim$n, sim$K, sim$D)
-o <- .SigmaK_fit(fit$SigmaKstar, fit$H, m)
+fit <- EDMAinR:::.edma_fit_np(sim$X, sim$n, sim$K, sim$D)
+o <- .SigmaK_fit(fit$SigmaKstar, fit$H, m, init=c(a=runif(0, 1), b=0))
+o
+
+replicate(10, .SigmaK_fit(fit$SigmaKstar, fit$H, m, init=c(a=runif(1), b=0))[1:2], simplify=F)
 
 cbind(true=parm, est=o$par)
+
