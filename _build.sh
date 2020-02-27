7aef86e681ec381feda20d76d5ce3c463af4b561
Peter Solymos <psolymos@gmail.com>
2020-02-26 23:25:55 -0700

    added rcmdcheck

--- _build.sh
+++ _build.sh
@@ -2,6 +2,7 @@
 
 set -ev
 
+Rscript -e "rcmdcheck::rcmdcheck(args = '--no-manual', error_on = 'warning', check_dir = 'check')"
 Rscript -e "pkgdown::build_site()"
 
 set -o errexit -o nounset
@@ -14,7 +15,7 @@ git config --global user.name "Peter Solymos"
 
 git clone -b gh-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git stats-output
 cd stats-output
-cp -r ../_stats/* ./
+cp -r ../docs/* ./
 git add --all *
 git commit -m "Update stats (${TRAVIS_BUILD_NUMBER})" || true
 git push -q origin gh-pages
