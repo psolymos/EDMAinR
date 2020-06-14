81476c77b150d4cd893b7938b14c1e2e6b98cb7b
Peter Solymos <psolymos@gmail.com>
2020-06-13 23:47:37 -0600

    as.edma_data method added

--- R/data.R
+++ R/data.R
@@ -266,3 +266,30 @@ as.dist.edma_data <- function(m, diag = FALSE, upper = FALSE) {
     out
 }
 
+as.edma_data <- function (x, ...) UseMethod("as.edma_data")
+
+as.edma_data.array <- function(x, ...) {
+    d <- dim(x)
+    K <- d[1L]
+    D <- d[2L]
+    n <- d[3L]
+    dn <- dimnames(x)
+    if (is.null(dn[[1L]]))
+        dn[[1L]] <- paste0("L", seq_len(K))
+    if (is.null(dn[[2L]]))
+        dn[[2L]] <- c("X", "Y", "Z")[seq_len(D)]
+    if (is.null(dn[[3L]]))
+        dn[[3L]] <- paste0("S", seq_len(n))
+    DATA <- list()
+    LM <- paste0("L", seq_len(K))
+    for (i in seq_len(n)) {
+        DATA[[i]] <- x[,,i]
+    }
+    names(DATA) <- dn[[3L]]
+    out <- list(
+        name="Landmark data",
+        data=DATA
+    )
+    class(out) <- c("edma_data")
+    out
+}
